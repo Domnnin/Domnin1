bc72971145d883e3717749e8e4a1827c39a2887b
Shubinat <shubinat729@gmail.com>
2022-05-23 17:00:18 +0300

    Добавьте файлы проекта.

---
+++ Domnin/Domnin/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Domnin.ViewModels;
+using Domnin.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Domnin
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ Domnin/Domnin/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Domnin.Models;
+using Domnin.ViewModels;
+using Domnin.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Domnin.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
